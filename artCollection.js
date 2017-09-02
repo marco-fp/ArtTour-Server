70a9e355b9af117c3809ac4c91280c78c2d0e921
rialba <mfernandezpranno@gmail.com>
2017-09-03 01:40:36 +0200

    Basic endpoint

---
+++ artCollection.js
@@ -0,0 +1,8 @@
+const artCollection = {
+  a: {
+    title: 'This is the title',
+    body: 'This is the body'
+  }
+};
+
+module.exports = artCollection;
---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var artCollection = require('./artCollection');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/info', function (req, res) {
+  const code = req.query.code;
+  console.log(artCollection);
+  if (!code) {
+    res.send({error: 'No code provided'});
+  } else {
+    const art = artCollection[code];
+    if (!art) {
+      res.send({error: 'Incorrect code provided'});
+    } else {
+      res.send({data: art});
+    }
+  }
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
